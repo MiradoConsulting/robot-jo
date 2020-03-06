82ece6bbb1d4f10509dc165622a5c1bab9595dcd
Sirvess <joe.slask@gmail.com>
2020-03-06 17:52:39 +0100

    derp

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -19,30 +19,37 @@ public class Hello extends Robot
 
 		 setColors(Color.pink,Color.black,Color.green); // body,gun,radar
 
+
 		// Robot main loop
 		while(true) {
 			// Replace the next 4 lines with any behavior you would like
-			ahead(100);
-			turnGunRight(360);
-			back(100);
-			turnGunRight(360);
+			drive();			
 		}
 	}
 
+	public void drive() {
+		turnGunRight(360);
+	}
 	/**
 	 * onScannedRobot: What to do when you see another robot
 	 */
 	public void onScannedRobot(ScannedRobotEvent e) {
-		// Replace the next line with any behavior you would like
-		fire(1);
+	   if (e.getDistance() < 100) {
+           fire(3);
+       } else {
+           fire(1);
+       }
+	   ahead(200);
 	}
+	
+
 
 	/**
 	 * onHitByBullet: What to do when you're hit by a bullet
 	 */
 	public void onHitByBullet(HitByBulletEvent e) {
 		// Replace the next line with any behavior you would like
-		back(10);
+		turnGunRight(360);
 	}
 	
 	/**
@@ -50,6 +57,12 @@ public class Hello extends Robot
 	 */
 	public void onHitWall(HitWallEvent e) {
 		// Replace the next line with any behavior you would like
-		back(20);
+		back(30);
+		turnRight(90);
+	}
+	
+	
+	public void onHitRobot(HitRobotEvent e) {	
+		turnGunRight(360);
 	}	
 }
